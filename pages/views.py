e8228348681644755b6ffcabc2283b263c34229f
deekshithanand <deekshith27anand@gmail.com>
2020-07-18 23:45:01 +0530

    'init1'

---
+++ pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from django.http import request,response,HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+    
